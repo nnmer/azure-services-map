8035f3dbd4e7e31fb3717be47b4c5bb6a7fbf346
Anton Romanov <anton.v.romanov@gmail.com>
2019-03-19 12:23:19 +0800

    D3LayoutHelpBox  and ServiceDirectedGraphModal components added

---
+++ src/components/pages/ServiceVsGroups/ServiceDirectedGraphModal.vue
@@ -0,0 +1,72 @@
+<template>
+  <b-modal
+    id="services-io-directed-graph"
+    title="Services Input/Output flow"
+    :no-fade="true"
+    :hide-footer="true"
+  >
+    <div class="svg-container">
+      <D3LayoutHelpBox/>
+      <div id="service-flow"></div>
+    </div>
+  </b-modal>
+</template>
+
+<script>
+import D3LayoutHelpBox from 'src/components/_components/D3LayoutHelpBox'
+import serviceFlowTree from 'src/_helpers/ServiceFlowTree'
+
+export default {
+  name: 'ServiceDirectedGraphModal',
+  components: {
+    D3LayoutHelpBox
+  },
+  mounted: function() {
+    this.$root.$on('app::services::io-directed-graph::show', this.showIOFlowGraph)
+  },
+  methods: {
+    showIOFlowGraph: function (event, serviceId) {
+      this.$root.$emit('bv::show::modal', 'services-io-directed-graph', event)
+      this.showServiceIntegrationTree(serviceId, SL.services)
+    },
+    showServiceIntegrationTree: function(serviceId, services) {
+      function buildChildren(id) {
+        // console.warn('Called buildChildren',id)
+        let service = services[id];
+        let obj = {
+          name: service ? service.name : id
+        }
+        circularRefTrack.push(id)
+
+        if (service && service.servicesIO.output.length > 0){
+          for (let i=0;i<service.servicesIO.output.length;i++){
+            let servId = service.servicesIO.output[i];
+            let depService = services[servId]
+            let toInject = null;
+            if (circularRefTrack.indexOf(servId) === -1 && depService) {
+              toInject = buildChildren(servId)
+            } else {
+              toInject = {
+                name: depService ? depService.name : servId
+              }
+              circularRefTrack.push(servId)
+            }
+            if (!obj.children){
+              obj.children = []
+            }
+            obj.children.push(toInject)
+            circularRefTrack.pop()
+          }
+        }
+
+        return obj;
+      }
+
+      var circularRefTrack = [];
+      $('#service-flow').html('');
+      let tree = buildChildren(serviceId)
+      serviceFlowTree(tree)
+    }
+  }
+}
+</script>

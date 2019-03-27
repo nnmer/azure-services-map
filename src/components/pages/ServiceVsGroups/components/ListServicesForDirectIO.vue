152790c29bf4f0db521b4427b539612f33cdd2b2
Anton Romanov <v-antonr@microsoft.com>
2019-03-27 18:43:40 +0800

    refactor direct io modal

--- src/components/pages/ServiceVsGroups/components/ListServicesForDirectIO.vue
+++ src/components/pages/ServiceVsGroups/components/ListServicesForDirectIO.vue
@@ -1,33 +1,46 @@
 <template>
   <div>
     <div class="list-group-item"
-      v-for="(item) in dataSource">
+      v-for="(ioItem) in dataSource">
 
-        <div v-if="item.serviceId && hasService(item.serviceId)">
-          <img
-            v-if="SL(item.serviceId).icon"
-            class="service-icon"
-            :src="SL(item.serviceId).icon"
-          />
-
-          {{item.aliasTitle || (SL(item.serviceId) ? SL(item.serviceId).name : item.serviceId)}}
-          <a :href="SL(item.serviceId).url" target="_blank">Service doc</a>
-          <span v-if="item.connectionDescriptionUrl">
-            |
-            <ConnectionDescriptionLinks
-              :dataSource="item.connectionDescriptionUrl"
+        <div v-if="ioItem.serviceId && hasService(ioItem.serviceId)">
+          <div class="pull-left">
+            <img
+              v-if="getServiceData(ioItem.serviceId).icon"
+              class="service-icon m-r-5"
+              :src="getServiceData(ioItem.serviceId).icon"
             />
-          </span>
+
+            <span v-if="hasService(ioItem.serviceId)" class="m-r-5">
+              <a href="#"
+                @click="$root.$emit('app::services::direct-io-modal::show', $event, ioItem.serviceId)"
+                >{{serviceRenderName(ioItem)}}</a>
+            </span>
+            <span v-else  class="m-r-5">
+              {{serviceRenderName(ioItem)}}
+            </span>
+          </div>
+          <div class="pull-right service-item-doc-links">
+            <a :href="getServiceData(ioItem.serviceId).url" target="_blank">Service doc</a>
+            <span v-if="ioItem.connectionDescriptionUrl">
+              |
+              <ConnectionDescriptionLinks
+                :dataSource="ioItem.connectionDescriptionUrl"
+              />
+            </span>
+          </div>
+          <div class="clearfix"/>
         </div>
         <div v-else>
-          {{item.aliasTitle}}
-          <span v-if="item.connectionDescriptionUrl">
-
+          <div class="pull-left">
+            {{ioItem.aliasTitle}}
+          </div>
+          <div v-if="ioItem.connectionDescriptionUrl" class="pull-right service-item-doc-links">
             <ConnectionDescriptionLinks
-              :dataSource="item.connectionDescriptionUrl"
+              :dataSource="ioItem.connectionDescriptionUrl"
             />
-
-          </span>
+          </div>
+          <div class="clearfix"/>
         </div>
     </div>
   </div>
@@ -45,11 +58,14 @@ export default {
     ConnectionDescriptionLinks
   },
   methods: {
-    SL: function(item) {
-      return SL.services[item]
+    getServiceData: function(serviceId) {
+      return SL.services[serviceId]
     },
-    hasService: function (item) {
-      return SL.services[item] || null
+    hasService: function (serviceId) {
+      return SL.services[serviceId] || null
+    },
+    serviceRenderName: function(ioItem) {
+      return ioItem.aliasTitle || (this.hasService(ioItem.serviceId) ? this.getServiceData(ioItem.serviceId).name : ioItem.serviceId)
     }
   }
 }

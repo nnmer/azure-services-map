f8039c12b511356e149f3c3fad5aed1e68d12d4a
Anton Romanov <anton.v.romanov@gmail.com>
2019-11-05 14:26:51 +0800

    new service azure-sql-database-edge

--- parse-services.js
+++ parse-services.js
@@ -30,7 +30,8 @@ var excludeItems = Array(
   'web-apps', // alias to app-service---web-apps
   'mobile-apps', // alias to app-service---mobile-apps,
   'azure-active-directory-domain-services', // alias to azure-active-directory-for-domain-services,
-  'virtual-machines' // alias to linux and windows virtual machines
+  'virtual-machines', // alias to linux and windows virtual machines
+  'peering-service' // this is not a real service directly from Azure
 )
 
 function getHtml () {

0a697ac322a5cf0fe7250b35220f888b11cc369e
Anton Romanov <anton.v.romanov@gmail.com>
2020-02-26 02:05:04 +0800

    add a humburger menu to the service and move it to right top corner

--- src/components/pages/ServiceVsGroups/ServiceVsGroupsTable.vue
+++ src/components/pages/ServiceVsGroups/ServiceVsGroupsTable.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="service-list-container data-container-table">
-    <div class="service-list row text-center m-none">
+    <div class="service-list scrollable row text-center m-none">
       <div
         :key="catIdx"
         class="service-list-col" v-for="(services, category, catIdx) in filteredServicesList" >
@@ -10,30 +10,43 @@
         <div :key="service.id"
             v-for="(service) in filteredServicesList[category]"
             v-bind:data-service-id="service.id"
-            v-bind:id="catIdx+'-'+service.id"
-            @click.capture="$root.$emit('app::services::popover::show', $event, service.id, catIdx+'-'+service.id)"
+            v-bind:id="catIdx+'-'+service.id"            
         >
-          <div class="service-list-col-service-item" :class="!serviceHasAvailability(service) ? 'no-region-availability' :''">
+          <div class="service-list-col-service-item">
             <div
-              class="pull-right"
+              class="pull-left"
             >
               <a v-if="serviceHasAvailability(service)"
                  href="#"
-                 class="glob-link"
+                 class="action-icon"
                  @click.stop="$root.$emit('app::services::region-availability-modal::show', $event, service.id)"
               >
-                <img src="/img/globe.png" width="14px"/>
+                <img src="/img/globe.png" width="16px"/>
+              </a>
+            </div>
+
+            <div
+              class="pull-right"
+            >
+              <a href="#" 
+                class="action-icon"
+                v-bind:id="`humb-menucat-${catIdx}-${service.id}`"
+                @click.capture="$root.$emit('app::services::popover::show', $event, service.id, `humb-menucat-${catIdx}-${service.id}`)"
+              >
+                <img src="/img/icon_humb.svg" width="16px"/>
               </a>
             </div>
             <div class="clearfix"/>
 
 
-            <img class="service-icon" v-bind:src="service.icon || 'img/icon-azure-black-default.png'"/>
-            <div
-              v-bind:class="{'has-linking-services':service.servicesIO.input && service.servicesIO.input.length >0
-              || service.servicesIO.output && service.servicesIO.output.length >0}"
-            >
-            {{service.name}}
+            <div class="m-t-7">
+              <img class="service-icon" v-bind:src="service.icon || 'img/icon-azure-black-default.png'"/>
+              <div
+                v-bind:class="{'service-title':true,'has-linking-services':service.servicesIO.input && service.servicesIO.input.length >0
+                || service.servicesIO.output && service.servicesIO.output.length >0}"
+              >
+              {{service.name}}
+              </div>
             </div>
           </div>
         </div>
@@ -43,6 +56,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'ServicesVsGroupsTable',
   props: [

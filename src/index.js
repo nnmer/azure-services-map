1f995c89002dd8faf9b962cd744f1b5768b3e862
Anton Romanov <anton.v.romanov@gmail.com>
2020-03-04 23:24:01 +0800

    import styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route } from "react-router-dom";
 import history from "src/helpers/history";
 import App from 'src/App';
-import 'src/styles';
+import 'src/styles/index.scss';
 import { routesUI } from './helpers/routing';
 
 ReactDOM.render(
